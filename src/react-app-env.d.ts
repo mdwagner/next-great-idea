12a80d8915fcde5282f57988a9e2200836524924
Michael Wagner <mdwranger@gmail.com>
2020-06-10 16:36:51 -0400

    refactor!: reverted fusionauth lib code
    - keeping docker config

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,13 +1,1 @@
 /// <reference types="react-scripts" />
-
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      REACT_APP_FUSIONAUTH_API_KEY: string;
-      REACT_APP_FUSIONAUTH_URL: string;
-      REACT_APP_FUSIONAUTH_TENANT?: string;
-    }
-  }
-}
-
-export {}
--- src/utils/fusionauthClient.ts
+++
@@ -1,7 +0,0 @@
-import { FusionAuthClient } from '@fusionauth/typescript-client';
-
-const clientApiKey = process.env.REACT_APP_FUSIONAUTH_API_KEY;
-const url = process.env.REACT_APP_FUSIONAUTH_URL;
-const tenantId = process.env.REACT_APP_FUSIONAUTH_TENANT;
-
-export const client = new FusionAuthClient(clientApiKey, url, tenantId);
