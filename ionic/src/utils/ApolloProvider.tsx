3841a5ee37049a7b531857444c0f48af1f463be4
Michael Wagner <mdwranger@gmail.com>
2020-07-03 01:03:41 +0000

    feat: tweaks to apollo provider

--- ionic/src/utils/ApolloProvider.tsx
+++ ionic/src/utils/ApolloProvider.tsx
@@ -11,21 +11,34 @@ const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: process.env.HASURA_GRAPHQL_ENDPOINT || "http://host.docker.internal:8080/v1/graphql",
   headers: {
-    'authorization': `Bearer ${window.localStorage.getItem('token') || ''}`,
-    'x-hasura-admin-secret': process.env.NODE_ENV === 'production' ? '' : 'secret'
+    'X-Hasura-Admin-Secret': process.env.NODE_ENV === 'production' ? '' : 'secret'
+  },
+  fetch: (input, init) => {
+    const token = window.localStorage.getItem('token');
+    const authHeader: object = token ? { 'Authorization': `Bearer ${token}` } : {};
+
+    return fetch(input, {
+      ...init,
+      headers: {
+        ...init?.headers,
+        ...authHeader
+      }
+    });
   }
 });
+
+const defaultCacheData = {
+  isLoggedIn: !!window.localStorage.getItem('token')
+};
+
+cache.writeData({ data: defaultCacheData });
 
 const client = new ApolloClient<NormalizedCacheObject>({
   cache,
   link
 });
 
-cache.writeData({
-  data: {
-    isLoggedIn: !!window.localStorage.getItem('token')
-  }
-});
+client.onResetStore(async () => cache.writeData({ data: defaultCacheData }));
 
 export const ApolloProvider: React.FC = ({ children }) => {
   return (
