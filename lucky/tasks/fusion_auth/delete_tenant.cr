b488854faf7908fbd6a01fddc4c0fdbbf18342b1
Michael Wagner <mdwranger@gmail.com>
2020-08-02 20:55:30 +0000

    feat: added delete tenant task

---
+++ lucky/tasks/fusion_auth/delete_tenant.cr
@@ -0,0 +1,9 @@
+class FusionAuth::DeleteTenant < LuckyCli::Task
+  summary "Delete NextGreatIdea Tenant in FusionAuth"
+  name "fa.delete_tenant"
+
+  def call
+    # TODO
+    abort "NOT_IMPLEMENTED"
+  end
+end
