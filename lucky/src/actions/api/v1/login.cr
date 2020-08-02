c3d42382a0785629c95d3498e4a7a2fd063eff51
Michael Wagner <mdwranger@gmail.com>
2020-08-02 02:51:05 +0000

    feat: renamed serializer to result in actions

--- lucky/src/actions/api/v1/login.cr
+++ lucky/src/actions/api/v1/login.cr
@@ -1,7 +1,7 @@
 class Api::V1::Login < ApiAction
   post "/api/v1/login" do
-    FusionAuthLogin.new(params).submit do |operation, serializer|
-      json serializer, status: operation.status.code
+    FusionAuthLogin.new(params).submit do |operation, result|
+      json result, status: operation.status.code
     end
   end
 end
--- lucky/src/actions/api/v1/sign_up.cr
+++ lucky/src/actions/api/v1/sign_up.cr
@@ -1,7 +1,7 @@
 class Api::V1::SignUp < ApiAction
   post "/api/v1/sign_up" do
-    FusionAuthSignUp.new(params).submit do |operation, serializer|
-      json serializer, status: operation.status.code
+    FusionAuthSignUp.new(params).submit do |operation, result|
+      json result, status: operation.status.code
     end
   end
 end
