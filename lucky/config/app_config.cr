c1a5361db72d17d6fd0752c39495abd29e329c6e
Michael Wagner <mdwranger@gmail.com>
2020-08-07 03:32:21 +0000

    feat(lucky): can create and delete tenant

--- lucky/config/app_config.cr
+++ lucky/config/app_config.cr
@@ -4,11 +4,21 @@ class AppConfig
     setting fusionauth_api_key : String
     setting fusionauth_tenant_id : String
     setting fusionauth_app_id : String
+    setting fusionauth_theme_id : String
+    setting fusionauth_jwt_key_id : String
 
     setting hasura_graphql_url : String
     setting hasura_graphql_admin_secret : String
 
     setting git_commit : String
+
+    setting email_host : String
+    setting email_port : String
+    setting issuer : String
+    setting app_name : String
+
+    setting jwt_kid : String
+    setting jwt_secret : String
   end
 end
 
@@ -17,10 +27,20 @@ AppConfig.configure do |settings|
   settings.fusionauth_api_key = ENV.fetch("FUSIONAUTH_API_KEY", "bf69486b-4733-4470-a592-f1bfce7af580")
   settings.fusionauth_tenant_id = ENV.fetch("FUSIONAUTH_TENANT_ID", "ec02c5c8-d4ef-440e-b5ff-5f3b4fe51b48")
   settings.fusionauth_app_id = ENV.fetch("FUSIONAUTH_APP_ID", "85a03867-dccf-4882-adde-1a79aeec50df")
+  settings.fusionauth_theme_id = ENV.fetch("FUSIONAUTH_THEME_ID", "d2c5b616-cdb7-4abb-97cc-4465a29f313f")
+  settings.fusionauth_jwt_key_id = ENV.fetch("FUSIONAUTH_JWT_KEY_ID", "29c14b28-fe3a-4db5-a49c-3fd30b5e4bce")
 
   settings.hasura_graphql_url = ENV.fetch("HASURA_GRAPHQL_ENDPOINT", "http://hasura:8080")
   settings.hasura_graphql_admin_secret = ENV.fetch("HASURA_GRAPHQL_ADMIN_SECRET", "secret")
 
   git_commit = `git rev-parse HEAD`.strip
   settings.git_commit = ENV.fetch("GIT_COMMIT", git_commit)
+
+  settings.email_host = ENV.fetch("EMAIL_HOST", "localhost")
+  settings.email_port = ENV.fetch("EMAIL_PORT", "3001")
+  settings.issuer = ENV.fetch("APP_ISSUER", "http://example.com")
+  settings.app_name = ENV.fetch("APP_NAME", "NextGreatIdea")
+
+  settings.jwt_kid = ENV.fetch("JWT_KID", "QTccbJfA2iI8rNHwUGYWdiNgrN-ueQOK")
+  settings.jwt_secret = ENV.fetch("JWT_SECRET", "secret")
 end
--- lucky/tasks/fusion_auth/delete_tenant.cr
+++ lucky/tasks/fusion_auth/delete_tenant.cr
@@ -1,7 +1,5 @@
-require "option_parser"
-
 class FusionAuth::DeleteTenant < LuckyCli::Task
-  summary "Delete NextGreatIdea Tenant in FusionAuth"
+  summary "Delete NextGreatIdea Tenant, Theme, and Keys in FusionAuth"
   name "fa.delete_tenant"
 
   def call
@@ -9,25 +7,42 @@ class FusionAuth::DeleteTenant < LuckyCli::Task
       abort "ERROR: Can't delete tenant in production!"
     end
 
-    tenant_id = ""
-
-    OptionParser.parse do |parser|
-      parser.on("--tenant_id=TENANT_ID", "Tenant ID to delete") do |value|
-        tenant_id = value
-      end
-    end
-
-    response = AppHttpClient.execute(HttpClient::FusionAuth) do |client|
+    tenant_response = AppHttpClient.execute(HttpClient::FusionAuth) do |client|
       client.before_request do |request|
         request.headers.delete("Accept")
         request.headers.delete("Content-Type")
       end
-      client.delete("/api/tenant/#{tenant_id}")
+      id = AppConfig.settings.fusionauth_tenant_id
+      client.delete("/api/tenant/#{id}")
     end
+    error_response(tenant_response, "tenant")
 
-    if response.status.ok?
-      puts "OK"
-    else
+    theme_response = AppHttpClient.execute(HttpClient::FusionAuth) do |client|
+      client.before_request do |request|
+        request.headers.delete("Accept")
+        request.headers.delete("Content-Type")
+      end
+      id = AppConfig.settings.fusionauth_theme_id
+      client.delete("/api/theme/#{id}")
+    end
+    error_response(theme_response, "theme")
+
+    jwt_key_response = AppHttpClient.execute(HttpClient::FusionAuth) do |client|
+      client.before_request do |request|
+        request.headers.delete("Accept")
+        request.headers.delete("Content-Type")
+      end
+      id = AppConfig.settings.fusionauth_jwt_key_id
+      client.delete("/api/key/#{id}")
+    end
+    error_response(jwt_key_response, "jwt key")
+
+    puts "OK"
+  end
+
+  def error_response(response, name : String? = nil)
+    if !response.status.ok?
+      puts "NAME: #{name}" if name
       puts "CODE: #{response.status_code}"
       puts "BODY: #{response.body}"
       abort
