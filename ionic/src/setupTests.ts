488aabf1cc0a13daf1320b94ebd00c891348575a
Michael Wagner <mdwranger@gmail.com>
2021-03-06 02:31:07 -0500

    fix: mock env vars in tests

--- ionic/src/setupTests.ts
+++ ionic/src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+// mock environment variables
+process.env.REACT_APP_GRAPHQL_URL = "http://localhost:8080/graphql";
+process.env.REACT_APP_ADMIN_SECRET = "secret";
